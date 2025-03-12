b3453a929bfe32874c9aca877027da450a314fe7
Hwang gend <ghkdtjsdks@naver.com>
2025-03-12 15:20:42 +0900

    chore: 불필요한 코드 정리

--- sunan/src/components/DrawingCanvas.jsx
+++ sunan/src/components/DrawingCanvas.jsx
@@ -5,19 +5,37 @@ import "../styles/components/DrawingCanvas.scss";
 import "../styles/pages/GamePage.scss";
 import MessageBox from "./MessageBox";
 //만약 페이지로 구현한다면 전역변수로 기존 props주던거 관리하면됨
-const DrawingCanvas = ({ timeLeft, roundNum, setRoundNum }) => {
+const DrawingCanvas = ({ roundNum, setRoundNum }) => {
   const [lines, setLines] = useState([]);
   const [isDrawing, setIsDrawing] = useState(false);
   const [tool, setTool] = useState("pen");
   const canvasRef = useRef(null);
   const stageRef = useRef(null);
+  const [timeLeft, setTimeLeft] = useState(30);
+
+  // 라운드가 변경될 때마다 시간 초기화
+  useEffect(() => {
+    setTimeLeft(30); // 새로운 라운드 시작 시 30초로 초기화
+  }, [roundNum]);
+
+  // 타이머 로직 (1초마다 감소)
+  useEffect(() => {
+    if (timeLeft <= 0) return; // 시간이 끝나면 멈춤
+
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+  //마우스 누르면 그림 그리기 시작
   const handleMouseDown = (e) => {
     if (timeLeft <= 0) return;
     setIsDrawing(true);
     const pos = e.target.getStage().getPointerPosition();
     setLines([...lines, { tool, points: [pos.x, pos.y] }]);
   };
-
+  //마우스 움직일 때마다 그림 그리기
   const handleMouseMove = (e) => {
     if (!isDrawing || timeLeft <= 0) return;
     const stage = e.target.getStage();
@@ -26,19 +44,16 @@ const DrawingCanvas = ({ timeLeft, roundNum, setRoundNum }) => {
     lastLine.points = [...lastLine.points, point.x, point.y];
     setLines([...lines]);
   };
-
+  //마우스 떼면 그리기 종료
   const handleMouseUp = () => {
     setIsDrawing(false);
   };
-
+  //캔버스 초기화(쓰레기통 기능)
   const handleClearCanvas = () => {
     setLines([]);
   };
-
+  //이걸로 미리 모델서버에 그림을 전송함
   const handleSendImage = () => {
-    console.log("try send image");
-
-    console.log("send image");
     const dataURL = stageRef.current.toDataURL({ mimeType: "image/png" });
 
     const byteString = atob(dataURL.split(",")[1]);
